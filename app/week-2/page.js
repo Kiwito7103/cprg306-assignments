8b4707a977cc91ec8e60d8b11df0e76f39f381c0
Kiwito <lyubstriletskyy@gmail.com>
2025-09-16 17:00:54 -0600

    finished in 1 commit
    quite the coder am i, haha

---
+++ app/week-2/page.js
@@ -0,0 +1,13 @@
+import Studentinfo from "./student-info";
+export default function Page() {
+
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <Studentinfo>
+
+      </Studentinfo>
+
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+function StudentInfo() {
+  return (
+    <div>
+      <h1>My Git stuffs</h1>
+      <p>Lyub Striletskyy</p>
+      <p>Github Link: <Link href="https://github.com/Kiwito7103/cprg306-assignments">https://github.com/Kiwito7103/cprg306-assignments</Link></p>
+
+    </div>
+  )
+}
+export default StudentInfo;
